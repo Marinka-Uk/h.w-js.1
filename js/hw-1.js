ac84dc075fe6c782e54fba759b78a353e914763b
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-05 18:48:24 +0200

    .

---
+++ js/hw-1.js
@@ -0,0 +1,31 @@
+const age = 15;
+console.log(age);
+
+const name = 'Marina';
+console.log(name);
+
+const isStudent = true;
+console.log(isStudent);
+
+const myString = "the best revenge is great success";
+console.log(myString);
+
+let myNumber = 13;
+myNumber = myNumber + 10;
+console.log(myNumber);
+
+const myNull = null;
+console.log(myNull);
+
+
+const yourName = prompt("Твоє ім'я?");
+console.log(yourName);
+alert('Привіт ' + yourName);
+confirm('Чи бажаєте ви оформити підписку?');
+
+alert('Наступна дія буде небезпечною');
+confirm("Ви впевнені що хочете продовжити?");
+
+
+
+
