b98eec4f15b19357d19358870f6ce4cc1d721c07
Marinka-Uk <129098497+Marinka-Uk@users.noreply.github.com>
2023-12-19 19:16:02 +0200

    .

---
+++ js/hw-2.js
@@ -0,0 +1,64 @@
+//1
+// const tempCels = 12;
+// const tempFar = tempCels * 9 / 5 + 32;
+// console.log(tempFar);
+
+// 25*9/5+32 = 77°F.
+
+//2 
+
+// const daysMnth = 31;
+// const hours = daysMnth * 24;
+// const minutes = 60 * hours;
+// console.log(hours, minutes);
+
+//3
+
+// let health = 10;
+// let energy = 10;
+// health -= 1;
+// energy -= 3;
+// console.log(health);
+// console.log(energy);
+
+
+
+//4
+
+// let price = 150;
+// console.log(price -= price * 10 / 100);
+
+
+//5
+// let number = 1.5;
+// Math.floor(number);
+// console.log(Math.floor(number));
+
+
+
+//6
+
+// const number = '2.6';
+// Number.parseFloat(number);
+// console.log(Number.parseFloat(number));
+
+//7 
+
+// const number = '15px';
+// Number.parseInt(number);
+// console.log(Number.parseInt(number));
+
+//8
+
+// const number = 9;
+// Math.sqrt(number);
+// console.log(Math.sqrt(number));
+
+//9 
+
+// const number = 60;
+// const strNumb = '10px';
+// Number.parseInt(strNumb);
+// number.toString();
+// console.log(typeof Number.parseInt(strNumb));
+// console.log(typeof number.toString());
---
+++ js/hw-3.js
@@ -0,0 +1,21 @@
+//1
+const result = 5 + 5 + '5';
+console.log(result);
+
+//2
+const email = '@marinka19';
+email.includes('@');
+console.log(email.includes('@'));
+console.log(email.length);
+
+//3
+const a = 'My ';
+const b = 'name ';
+const c = 'is ';
+const fullName = a + b + c + 'Victor';
+console.log(fullName);
+
+//4
+const userName = 'Marik! '
+const payment = '300 гривень';
+alert('Дякуємо, ' + userName + 'До сплати ' + payment );
